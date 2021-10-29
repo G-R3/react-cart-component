e28d240f76578af53e2cd4c75e3f703a1d4923fc
Gerardo <gr261212@gmail.com>
2021-10-29 18:59:45 -0400

    update cart item compoent with onClick

--- src/components/Cartitem.js
+++ src/components/Cartitem.js
@@ -3,20 +3,29 @@ import '../styles/Cartitem.css';
 
 class Cartitem extends Component {
   render() {
-    const { name, price, qty } = this.props;
+    const { index, name, price, qty, incrementQty, decrementQty } = this.props;
     return (
       <div className="Cartitem">
         <h3 className="Cartitem-name">{name}</h3>
         <div className="Cartitem-options">
-          <button className="Cartitem-decrement" type="button">
+          <button
+            className="Cartitem-decrement"
+            type="button"
+            onClick={() => decrementQty(index)}
+            disabled={qty <= 1}
+          >
             -
           </button>
           <p className="Cartitem-qty">{qty}</p>
-          <button className="Cartitem-increment" type="button">
+          <button
+            className="Cartitem-increment"
+            type="button"
+            onClick={() => incrementQty(index)}
+          >
             +
           </button>
         </div>
-        <p className="Cartitem-price">${price}</p>
+        <p className="Cartitem-price">${price * qty}</p>
       </div>
     );
   }
